794f104d6c7f1d5854b618c5b89a0e09dfe4d986
kite <166883807+Kite1219@users.noreply.github.com>
2025-06-18 12:29:29 -0400

    Only load env file when variables unset

--- start.sh
+++ start.sh
@@ -2,7 +2,10 @@
 set -e
 
 echo "run db migration"
-source /app/app.env
+# Load environment variables if they are not already set
+if [ -z "$DB_SOURCE" ]; then
+  . /app/app.env
+fi
 migrate -path /app/migration -database "$DB_SOURCE" -verbose up   # ← use “migrate”, not “/app/migrate”
 
 echo "start the app"
